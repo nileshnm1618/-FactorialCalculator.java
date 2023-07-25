50c22a80b55188c2c855840c3b1c4356e08f2210
Nilesh <nileshnm1618@gmail.com>
2023-07-25 14:55:27 +0530

    first commit for factorial calculator

---
+++ FactorialCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    private static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
